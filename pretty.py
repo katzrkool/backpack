22e44c7d528a2ed774bc47b2364ccb69d08327c7
Lucas <katzrkool@protonmail.com>
2018-08-14 15:00:25 -0500

    parser acting weird

--- pretty.py
+++ pretty.py
@@ -4,16 +4,17 @@ import re
 def prettify(webData):
     soup = BeautifulSoup(webData, "html.parser")
 
-    classes = [i.get_text() for i in soup.find_all(class_="dailyGradeCourseNameColumn")]
-    grades = [re.sub(r'^\xa0$', 'N/A', i.get_text().split(": ")[1]) for i in soup.find_all(class_="dailyGradeGroupColumn")]
-
-    assignments = []
+    data = []
 
     for i in soup.find_all(class_="rich-panel-body"):
+        course = i.find(class_="dailyGradeCourseNameColumn").get_text()
+        grade = re.sub(r'^\xa0$', 'N/A', i.find(class_="dailyGradeGroupColumn").get_text().split(": ")[1])
         assignmentNames = [x.get_text() for x in i.find_all(class_="assignmentName")]
         assignmentScores = [x.get_text() for x in i.find_all(class_="points")]
-        assignments.append({assignmentNames[x]: assignmentScores[x] for x in range(0, len(assignmentNames))})
-
-    data = [{'class': classes[i], 'grade': grades[i], 'assignments': assignments[i]} for i in range(0, len(classes))]
-
+        data.append({
+            'class': course,
+            'grade': grade,
+            'assignments': {assignmentNames[x]: assignmentScores[x] for x in
+             range(0, len(assignmentNames))}})
+    
     return data
